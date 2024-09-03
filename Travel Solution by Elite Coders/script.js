836a97c0eca201c283ceacba681aec2c3f1a2e8d
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2024-09-03 19:00:09 +0530

    Add files via upload

--- Travel Solution by Elite Coders/script.js
+++ Travel Solution by Elite Coders/script.js
@@ -2,7 +2,23 @@ const startLocation = document.querySelector('#startloc');
 const destination = document.querySelector('#endloc');
 const searchButton = document.querySelector('#searchButton');
 const display=document.querySelector('.display');
-searchButton.addEventListener('click', function() {
+const hamButton=document.querySelector('.ham');
+const nav=document.querySelector('nav .right');
+/* For displaying Locations */
+function displayLocations(){
     display.innerHTML=`<p> start destinantion : ${startLocation.value}</p>
         <p>end destinantion : ${destination.value}</p>`;
+}
+
+/* For toggle Nav */
+hamButton.addEventListener('click',function()
+{
+    nav.classList.toggle('show');
+    console.log('nav buton')
+})
+
+
+
+searchButton.addEventListener('click', function() {
+    displayLocations();
 });
